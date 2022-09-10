c3e4e601960a0ee0b5931d8ce32c9d978d89327d
Michael Wood <michaelwood33311@icloud.com>
2022-09-10 09:42:37 -0400

    added creating manual playlists using array of media ids

--- jwplayer.js
+++ jwplayer.js
@@ -1,3 +1,5 @@
+//https://developer.jwplayer.com/jwplayer/reference/get_v2-sites-site-id-media
+
 require('dotenv').config();
 const axios = require('axios');
 
@@ -6,13 +8,15 @@ const siteId = process.env.SITE_ID;
 
 console.log(siteId, secret);
 
-const getMediaList = (pageNumber, numPerPage) => {
+const getMediaList = (pageNumber = 1, numPerPage = 10000) => {
     return new Promise((resolve, reject) => {
         const request = {
             url: `https://api.jwplayer.com/v2/sites/${siteId}/media/`,
             method: "get",
-            page: pageNumber,
-            page_length: numPerPage,
+            params: {
+                page: pageNumber,
+                page_length: numPerPage,
+            },
             headers: {
                 Authorization: `Bearer ${secret}`
             }
@@ -24,6 +28,32 @@ const getMediaList = (pageNumber, numPerPage) => {
     })
 }
 
-getMediaList(1, 12)
-.then(data => console.log(data))
-.catch(err => console.error(err))
+const createManualPlaylist = mediaList => {
+    return new Promise((resolve, reject) => {
+        const request = {
+            url: `https://api.jwplayer.com/v2/sites/${siteId}/playlists/manual_playlist`,
+            method: "post",
+            data: {
+                metadata: {
+                    media_filter: {include: {match: 'any', values: mediaList}},
+                    title: 'Hello',
+                    author: 'PYMNTS'
+                }
+            },
+            headers: {
+                Authorization: `Bearer ${secret}`
+            }
+        }
+        axios(request)
+        .then(response => resolve(response.data))
+        .catch(error => reject(error))
+    })
+}
+
+// getMediaList()
+// .then(data => console.log(data))
+// .catch(err => console.error(err))
+
+createManualPlaylist(['cnLuyfOm', 'zb6VSIAe'])
+.then(response => console.log(response))
+.catch(error => console.error(error))
